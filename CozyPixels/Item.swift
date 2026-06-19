5634a5975704b43f02f2b78f51d9632407d171ed
Mike Repeć <mikerepec@pm.me>
2026-06-19 21:20:44 +0200

    Initial Commit

---
+++ CozyPixels/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CozyPixels
+//
+//  Created by Michał Repeć on 19/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
